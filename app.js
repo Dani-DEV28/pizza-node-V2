20ec6b5af2d161b57267f6731a26715aa2455e87
193554832+Dani-DEV28@users.noreply.github.com <193554832+Dani-DEV28@users.noreply.github.com>
2025-02-20 13:51:49 -0800

    update fixed

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 //Import Express
 import express from 'express';
+import mariadb from 'mariadb';
+
+const pool = mariadb.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'pizza'
+});
+
+async function connect() {
+    try{
+        const conn = await pool.getConnection();
+        console.log('Connected to the database');
+        return conn;
+    }catch (err){
+        console.log(`Error connecting to the database ${err}`);
+    }
+}
 
 //Instantiate an Express application
 const app = express();
@@ -17,7 +35,17 @@ const PORT = 3000;
 app.get('/', (req, res) => {
 
     // Send our home page as a response to the client
-    res.sendFile(`${import.meta.dirname}/views/home.html`);
+    res.render('home');
+});
+
+//Define an admin route
+app.get('/admin', async (req, res) => {
+    const conn = await connect();
+    const orders = await conn.query('SELECT * FROM orders;')
+
+    console.log(orders);
+
+    res.render('order-summary', { orders });
 });
 
 //Define a "thank you" route
